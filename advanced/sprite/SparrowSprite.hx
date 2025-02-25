f99cd076e3e0d287932dd67ee9296964e1ec6478
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-24 18:43:25 -0600

    SparrowSprite

---
+++ advanced/sprite/SparrowSprite.hx
@@ -0,0 +1,23 @@
+package advanced.sprite;
+
+import flixel.FlxSprite;
+import flixel.graphics.frames.FlxAtlasFrames;
+
+class SparrowSprite extends FlxSprite
+{
+        override public function new(Imagepath:String = '', ?X:Float = 0, ?Y:Float = 0) {
+                super(X,Y);
+
+                frames = FlxAtlasFrames.fromSparrow('$Imagepath.png', '$Imagepath.xml');
+        }
+
+        public function addAnimationByPrefix(name:String, prefix:String, frameRate = 30.0, looped = true, flipX = false, flipY = false)
+        {
+                animation.addByPrefix(name, prefix, frameRate, looped, flipX, flipY);
+        }
+
+        public function playAnimation(animName:String, force = false, reversed = false, frame = 0)
+        {
+                animation.play(animName, force, reversed, frame);
+        }
+}
